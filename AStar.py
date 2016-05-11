3d49dcbca43de828eadf70c1c58a4b90fb719aea
mstavares <hmigueltavares@gmail.com>
2016-05-12 00:53:57 +0100

    o core aparenta estar ok

--- AStar.py
+++ AStar.py
@@ -1,7 +1,77 @@
+class no:
+
+    def __init__(self, posicao, custo = 0):
+        self.posicao = posicao
+        self.custo = custo
+
+    def get_posicao(self):
+        return self.posicao
+
+    def get_custo(self):
+        return self.custo
+
+    def atualiza_custo(self, goal):
+        self.custo += 1 + calcula_heuristica(self, goal)
+        print self.posicao
+        print self.custo
+
+def devolve_a_posicao(lista):
+    return [i for i, x in enumerate(lista) if x == devolve_o_melhor_no(lista)][0]
+
+def devolve_o_melhor_no(lista):
+    melhor = None
+    for x in lista:
+        if(melhor is None):
+            melhor = x
+        else:
+            if(melhor.get_custo() < x.get_custo()):
+                continue
+            else:
+                melhor = x
+    return melhor
+
 def inicializa_espaco(dim):
     matriz = [ [ "0" for i in range(dim) ] for j in range(dim) ]
     matriz[0][1] = "1"
     for i in matriz:
         print " ".join(i)
 
+
+def calcula_heuristica(current, goal):
+    return abs(goal.get_posicao()[0] - current.get_posicao()[0]) + abs(goal.get_posicao()[1] - current.get_posicao()[1])
+
+def is_goal(current, goal):
+    if(current.get_posicao() == goal.get_posicao()):
+        return True
+    else:
+        return False
+
+def calcula_custo(q, goal):
+    for x in q:
+        x.atualiza_custo(goal)
+
+def calcula_expancao(current):
+    return  [no([current.get_posicao()[0] + 1, current.get_posicao()[1]]), \
+            no([current.get_posicao()[0] - 1, current.get_posicao()[1]]), \
+            no([current.get_posicao()[0], current.get_posicao()[1] + 1]), \
+            no([current.get_posicao()[0], current.get_posicao()[1] - 1])]
+
+def algoritmo(inicio, goal, espaco):
+    q = [inicio]
+    i = 0
+
+    while(len(q) > 0):
+        h = q.pop(devolve_a_posicao(q))
+        r = q
+        if(is_goal(h, goal)):
+            print "encontrei em %d iteracoes" %i
+            break
+        else:
+            q = calcula_expancao(h)
+            calcula_custo(q, goal)
+            q.extend(r)
+            i += 1
+
+
 inicializa_espaco(6)
+algoritmo(no([0, 0]), no([4, 5]), 2)
