27fffac09a6d0afabb4449efd04f651c8dd9ff65
Diogo Guiomar <diogo.m.v.guiomar@gmail.com>
2016-05-17 13:31:32 +0100

    Revert "vs project"
    This reverts commit 264ce5052c0b73506668115d9f8127a95ce00c9e.

--- AStar - Copy.py
+++
@@ -1,7 +0,0 @@
-def inicializa_espaco(dim):
-    matriz = [ [ "0" for i in range(dim) ] for j in range(dim) ]
-    matriz[0][1] = "1"
-    for i in matriz:
-        print " ".join(i)
-
-inicializa_espaco(5)
